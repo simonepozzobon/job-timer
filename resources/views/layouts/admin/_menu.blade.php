7ac0f96eaa7670a76ff19b05a4060a9dec812813
simone pozzobon <info@simonepozzobon.com>
2017-12-05 05:53:29 +0100

    Update new layout

--- resources/views/layouts/admin/_menu.blade.php
+++ resources/views/layouts/admin/_menu.blade.php
@@ -4,15 +4,22 @@
     </div>
     <nav class="navbar">
         <ul class="navbar-nav">
-          <li class="nav-item active">
-              <a class="nav-link" href="{{ route('admin.todo-status.index') }}">Modifica Stati</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="{{ route('admin.users.index') }}">Utenti</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="{{ route('admin.users.index') }}">Utenti</a>
-          </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('admin.todo-status.index') }}">Modifica Stati</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.users.index') }}">Utenti</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.users.index') }}">Utenti</a>
+            </li>
         </ul>
     </nav>
+    <div class="right-menu-bg">
+        <div class="icon">
+            <a href="#">
+                <i class="fa fa-plus"></i>
+            </a>
+        </div>
+    </div>
 </div>
--- resources/views/layouts/admin/_sidebar.blade.php
+++ resources/views/layouts/admin/_sidebar.blade.php
@@ -1,14 +1,14 @@
 <nav id="sidebar-menu" class="navbar">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.todo-status.index') }}">
-          @include('icons.projects')
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.users.index') }}">
-          @include('icons.users')
-        </a>
-    </li>
-  </ul>
+    <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="{{ route('admin.todo-status.index') }}">
+                @include('icons.projects')
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.users.index') }}">
+                @include('icons.users')
+            </a>
+        </li>
+    </ul>
 </nav>
